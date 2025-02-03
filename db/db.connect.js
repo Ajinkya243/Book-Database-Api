a04730ec094520e5b4c1f98c63d8a0451eaa0d22
Ajinkya243 <ajinkyagund2@gmail.com>
2025-02-03 18:50:25 +0530

    env changes

--- db/db.connect.js
+++ db/db.connect.js
@@ -1,6 +1,7 @@
 const mongoose=require('mongoose');
 require('dotenv').config();
-const mongo=process.env.MONGODB;
+const mongo="mongodb+srv://ajinkyagund2:ajinkya741@ajinkya-cluster.ccgy0.mongodb.net/?retryWrites=true&w=majority&appName=Ajinkya-Cluster"
+
 
 const initializeDatabase=async()=>{
     await mongoose.connect(mongo).then(()=>console.log("Database Connected.")).catch(error=>console.log('Database not connected.'))
